1bd3f9c399acf43dca8a03442af66fb0cdf66d6d
SvetlanaMinich <imsvetam@mail.ru>
2023-06-13 12:01:25 +0300

    hashTable insert, remove, search

--- hashtable.cpp
+++ hashtable.cpp
@@ -1,54 +1,83 @@
 #include "hashtable.h"
 
-HashTable::HashTable(int _size)
+template<size_t K>
+HashTable<K>::HashTable()
 {
-    table = new Node[_size];
-    size = _size;
+    table = new Node[K];
+    size = 0;
     c1 = 1;
     c2 = 1;
 }
-int HashTable::hashFunction(int _key)
+template<size_t K>
+size_t HashTable<K>::hashFunction(int _key)
 {
     return _key%size;
 }
-int HashTable::quadraticHashFunction(int _key, int i) {
+template<size_t K>
+size_t HashTable<K>::quadraticHashFunction(int _key, int i) {
     return (hashFunction(_key) + c1 * i + c2 * i * i) % size;
 }
-void HashTable::insert(int _key, QString _value)
+template<size_t K>
+void HashTable<K>::insert(int _key, QString _value)
 {
-    int index = hashFunction(_key);
-    int i = 1;
-    while (table[index].value!="") {
-        index = quadraticHashFunction(_key, i);
-        i++;
+    if(size==K)
+    {
+        throw std::out_of_range("cannot insert (there is no space in the table)");
     }
-    table[index].key = _key;
-    table[index].value = _value;
-}
-void HashTable::remove(int _key)
-{
-    int index = hashFunction(_key);
-    int i = 1;
-    while (table[index].value!="") {
-        if (table[index].key == _key) {
-            table[index].key = -1;  // Помечаем удаленный элемент
-            table[index].value = "";  // Очищаем значение
+    size_t i = hashFunction(_key);
+    size_t j = i;
+    int step = 1;
+    while (table[j].is_used)
+    {
+        if (table[j].key == _key)
+        {
+            table[j].value = _value;
             return;
         }
-        index = quadraticHashFunction(_key, i);
-        i++;
+        j = quadraticHashFunction(_key, step++);
+        if (j == i)
+            throw std::runtime_error("HashTable is full.");
+    }
+    table[j].key = _key;
+    table[j].value = _value;
+    table[j].is_used = true;
+    size++;
+}
+template<size_t K>
+void HashTable<K>::remove(int _key)
+{
+    size_t i = hashFunction(_key);
+    size_t j = i;
+    int step = 1;
+    while (table[j].is_used)
+    {
+        if (table[j].key == _key)
+        {
+            table[j].value = "";
+            table[j].is_used = false;
+            size--;
+            return;
+        }
+        j = quadraticHashFunction(_key, step++);
+        if (j == i)
+            break;
     }
 }
-QString HashTable::search(int _key)
+template<size_t K>
+QString HashTable<K>::search(int _key)
 {
-    int index = hashFunction(_key);
-    int i = 1;
-    while (table[index].value!="") {
-        if (table[index].key == _key) {
-            return table[index].value;
+    size_t i = hashFunction(_key);
+    size_t j = i;
+    int step = 1;
+    while (table[j].is_used)
+    {
+        if (table[j].key == _key)
+        {
+            return table[j].value;
         }
-        index = quadraticHashFunction(_key, i);
-        i++;
+        j = quadraticHashFunction(_key, step++);
+        if (j == i)
+            break;
     }
     return "";
 }
--- hashtable.h
+++ hashtable.h
@@ -1,25 +1,28 @@
 #ifndef HASHTABLE_H
 #define HASHTABLE_H
 #include <QString>
+#include <iostream>
 
 struct Node
 {
     int key;
     QString value;
     bool is_used;
+    Node():key(-1),value(""),is_used(false){}
+    Node(int _key, QString _value):key(_key),value(_value),is_used(true){}
 };
 
-
+template<size_t K = 2048>
 class HashTable
 {
 private:
     Node* table;
-    int size;
+    size_t size;
     int c1, c2;
 public:
-    HashTable(int _size);
-    int hashFunction(int _key);
-    int quadraticHashFunction(int _key, int i);
+    HashTable();
+    size_t hashFunction(int _key);
+    size_t quadraticHashFunction(int _key, int i);
     void insert(int _key, QString _value);
     void remove(int _key);
     QString search(int _key);
